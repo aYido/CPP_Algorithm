e9288430676c29bc8a1b4ae86fe11dd95f26f877
aYido <ayido0724@gmail.com>
2018-07-24 10:50:24 +0900

    BOJ Step02 10869

---
+++ BOJ/Step02/10869.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int A, B;
+    scanf_s("%d %d", &A, &B);
+    printf("%d\n", A + B);
+    printf("%d\n", A - B);
+    printf("%d\n", A * B);
+    printf("%d\n", A / B);
+    printf("%d\n", A % B);
+}
